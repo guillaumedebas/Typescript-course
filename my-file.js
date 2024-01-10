58e7e7702e6068949de9d73c07aca0084efd727a
Guillaume Debas <guillaumedebas@hotmail.fr>
2024-01-10 23:54:37 +0100

    object type

--- my-file.js
+++ my-file.js
@@ -1,5 +1,13 @@
+//variable
 function addition(x, y) {
     return x + y;
 }
 const result = addition(10, 20);
+console.log(result);
+//object
+function damage(characterToDamage, amount) {
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+const resultObject = damage({ life: 100 }, 12);
 console.log(result);
--- my-file.ts
+++ my-file.ts
@@ -1,5 +1,14 @@
+//variable
 function addition(x: number, y: number): number {
     return x + y;
 }
 const result: number = addition(10, 20);
+console.log(result);
+
+//object
+function damage(characterToDamage: { life: number}, amount: number): number {
+    characterToDamage.life -= amount;
+    return characterToDamage.life;
+}
+const resultObject = damage({ life: 100 }, 12);
 console.log(result);
